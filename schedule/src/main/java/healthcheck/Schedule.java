f968ccb15c1df207360482e0bde619dd5deb6175
JiyoungKim1372 <82069747+JiyoungKim1372@users.noreply.github.com>
2021-07-01 10:11:39 +0900

    Add files via upload

--- schedule/src/main/java/healthcheck/Schedule.java
+++ schedule/src/main/java/healthcheck/Schedule.java
@@ -22,6 +22,7 @@ public class Schedule {
         scheduleResistered.publishAfterCommit();
         
         // 요청처리 쓰레드에서 처리시간이 610 밀리가 넘어서기 시작하여 어느정도 유지되면 CB 회로가 닫히도록 (요청을 빠르게 실패처리, 차단) 설정
+        /*
         try {
             System.out.println("#####  schedule service 부하  #####");
             Thread.currentThread().sleep((long) (400 + Math.random() * 220));
@@ -29,7 +30,7 @@ public class Schedule {
         } catch (InterruptedException e) {
                 e.printStackTrace();
         }
-
+        */
     }
 
     @PostUpdate
